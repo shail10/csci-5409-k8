72d7762f3ba09ebd3226b80f19971d75c504b960
shail10 <shailkardani10@gmail.com>
2025-03-11 00:24:36 -0300

    Deploying on GKE, Test-6: CrashLoopBackOff error

--- program2/program2.js
+++ program2/program2.js
@@ -5,83 +5,81 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 8000
 
-const STORAGE_PATH = '/shail_pv_volume_k8'
-
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Container 2 is running!')
+  res.send('App is running!')
 })
 
-app.post('/sum', (req, res) => {
-  const { file, product } = req.body
+// app.post('/sum', (req, res) => {
+//   const { file, product } = req.body
 
-  try {
-    const filePath = path.join(STORAGE_PATH, file)
+//   try {
+//     const filePath = path.join('/data/', file)
 
-    const data = fs.readFileSync(filePath, 'utf8')
-    const rows = data.trim().split('\n')
+//     const data = fs.readFileSync(filePath, 'utf8')
+//     const rows = data.trim().split('\n')
 
-    if (rows.length < 2) {
-      return res.status(400).json({
-        file: file,
-        error: 'Input file not in CSV format',
-      })
-    }
+//     if (rows.length < 2) {
+//       return res.status(400).json({
+//         file: file,
+//         error: 'Input file not in CSV format',
+//       })
+//     }
 
-    const headers = rows[0].split(',')
+//     const headers = rows[0].split(',')
 
-    if (
-      headers.length !== 2 ||
-      headers[0] !== 'product' ||
-      headers[1] !== 'amount'
-    ) {
-      return res.status(400).json({
-        file: file,
-        error: 'Input file not in CSV format.',
-      })
-    }
+//     if (
+//       headers.length !== 2 ||
+//       headers[0] !== 'product' ||
+//       headers[1] !== 'amount'
+//     ) {
+//       return res.status(400).json({
+//         file: file,
+//         error: 'Input file not in CSV format.',
+//       })
+//     }
 
-    const productIndex = headers.indexOf('product')
-    const amountIndex = headers.indexOf('amount')
+//     const productIndex = headers.indexOf('product')
+//     const amountIndex = headers.indexOf('amount')
 
-    let sum = 0
-    for (let i = 1; i < rows.length; i++) {
-      const columns = rows[i].split(',')
+//     let sum = 0
+//     for (let i = 1; i < rows.length; i++) {
+//       const columns = rows[i].split(',')
 
-      if (columns.length !== 2) {
-        return res.status(400).json({
-          file: file,
-          error: 'Input file not in CSV format.',
-        })
-      }
+//       if (columns.length !== 2) {
+//         return res.status(400).json({
+//           file: file,
+//           error: 'Input file not in CSV format.',
+//         })
+//       }
 
-      const amount = parseFloat(columns[amountIndex])
-      if (isNaN(amount)) {
-        return res.status(400).json({
-          file: file,
-          error: 'Input file not in CSV format.',
-        })
-      }
+//       const amount = parseFloat(columns[amountIndex])
+//       if (isNaN(amount)) {
+//         return res.status(400).json({
+//           file: file,
+//           error: 'Input file not in CSV format.',
+//         })
+//       }
 
-      if (columns[productIndex] === product) {
-        sum += amount
-      }
-    }
+//       if (columns[productIndex] === product) {
+//         sum += amount
+//       }
+//     }
 
-    return res.json({
-      file: file,
-      sum: sum,
-    })
-  } catch (err) {
-    console.log(err)
-    return res.status(500).json({
-      file: file,
-      error: 'An error occurred while processing the file.',
-    })
-  }
-})
+//     return res.json({
+//       file: file,
+//       sum: sum,
+//     })
+//   } catch (err) {
+//     console.log(err)
+//     return res.status(500).json({
+//       file: file,
+//       error: 'An error occurred while processing the file.',
+//     })
+//   }
+// })
 
 app.listen(PORT, () => {
-  console.log(`Container 2 running on port:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`)
 })
